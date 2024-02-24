140793455a934fe10c4426ac767f3f780be96d24
yuu <tnky3704@gmail.com>
2024-02-25 01:47:41 +0900

    first commit

---
+++ src/main/java/com/codestep/TodOappApplication.java
@@ -0,0 +1,13 @@
+package com.codestep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodOappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodOappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestep/TodOappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodOappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
