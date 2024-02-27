133d0b7e1c8549bd060f16b219faad6778f060f3
yuu <tnky3704@gmail.com>
2024-02-28 00:21:36 +0900

    feat: メソッドセキュリティ使用のため、PreAuthorizeを設定

--- src/main/java/com/codestep/TODOapp/TODOappController.java
+++ src/main/java/com/codestep/TODOapp/TODOappController.java
@@ -1,5 +1,6 @@
 package com.codestep.TODOapp;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class TODOappController {
 	
 	@RequestMapping("/")
+	@PreAuthorize("permitAll")
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("title", "index page");
@@ -18,6 +20,7 @@ public class TODOappController {
 	}
 	
 	@RequestMapping("/secret")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ModelAndView secret(ModelAndView mav, HttpServletRequest request) {
 		String user = request.getRemoteUser();
 		String msg = "This is secret page. [login by \"" + user + "\"]";
