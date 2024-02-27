a1b6a047931d26f0f0c3ff20afdae687f74b8ec6
yuu <tnky3704@gmail.com>
2024-02-28 01:21:14 +0900

    feat: ログインページのリクエストハンドラを追加

--- src/main/java/com/codestep/TODOapp/TODOappController.java
+++ src/main/java/com/codestep/TODOapp/TODOappController.java
@@ -3,6 +3,7 @@ package com.codestep.TODOapp;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -27,6 +28,19 @@ public class TODOappController {
 		mav.setViewName("Secret");
 		mav.addObject("title", "Secret page");
 		mav.addObject("msg", msg);
+		return mav;
+	}
+	
+	@RequestMapping("/login")
+	@PreAuthorize("permitAll")
+	public ModelAndView login(ModelAndView mav, @RequestParam(value="error", required=false)String error) {
+		mav.setViewName("login");
+		if (error != null) {
+			mav.addObject("msg", "ログインできませんでした。");
+		} else {
+			mav.addObject("msg","ユーザー名とパスワードを入力");
+		}
+		
 		return mav;
 	}
 }
