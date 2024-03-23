e6cd6af38bb29190fde62404dfc737c52a761922
yuu <tnky3704@gmail.com>
2024-03-24 02:13:06 +0900

    feat: TODO作成用コントローラークラスとサービスクラスを実装。

--- src/main/java/com/codestep/TODOapp/Controllers/TODOController.java
+++ src/main/java/com/codestep/TODOapp/Controllers/TODOController.java
@@ -1,15 +1,25 @@
 package com.codestep.TODOapp.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.codestep.TODOapp.TODOItem;
+import com.codestep.TODOapp.Repositories.TODOService;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
-public class TODOappController {
+public class TODOController {
 	
 	@RequestMapping("/")
 	@PreAuthorize("permitAll")
@@ -52,6 +62,33 @@ public class TODOappController {
 			mav.addObject("msg","ユーザー名とパスワードを入力");
 		}
 		
+		return mav;
+	}
+	
+	@Autowired
+	TODOService TODOService;
+	
+	@GetMapping("/add")
+	@PreAuthorize("isAuthenticated()")
+	public ModelAndView showAddForm(ModelAndView mav, @ModelAttribute TODOItem item) {
+		mav.addObject("item", item);
+		mav.setViewName("/add");
+		return mav;
+	}
+	
+	@PostMapping("/add")
+	@PreAuthorize("isAuthenticated()")
+	public ModelAndView PostAddForm(ModelAndView mav, @ModelAttribute("item") @Validated TODOItem item, BindingResult result) {
+		if (result.hasErrors()) {
+			mav.addObject("item", item);
+			mav.setViewName("/add");
+			return mav;
+		}
+		
+		String name = SecurityContextHolder.getContext().getAuthentication().getName(); 
+		TODOService.create(name, item);
+		mav.addObject("item", item);
+		mav.setViewName("/add");
 		return mav;
 	}
 }
---
+++ src/main/java/com/codestep/TODOapp/Repositories/TODOService.java
@@ -0,0 +1,26 @@
+package com.codestep.TODOapp.Repositories;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.codestep.TODOapp.TODOItem;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class TODOService {
+	
+	@Autowired
+	TODORepository TODORepository;
+	
+	public TODOItem create(String username, TODOItem item) {
+		item.setName(username);
+		item.setCreated(LocalDate.now());
+		item.setDone(0);
+		TODORepository.saveAndFlush(item);
+		return item;
+	}
+}
