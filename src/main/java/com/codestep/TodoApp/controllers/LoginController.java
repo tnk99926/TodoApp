c182e48c18cd8e71833e7d1f50c3a0e151c3ea14
yuu <tnky3704@gmail.com>
2024-04-29 01:17:25 +0900

    update: 不要になったコントローラーを削除。permitAllを指定していたPreAuthoriozeを削除

--- src/main/java/com/codestep/TodoApp/controllers/LoginController.java
+++ src/main/java/com/codestep/TodoApp/controllers/LoginController.java
@@ -1,47 +1,20 @@
 package com.codestep.TodoApp.controllers;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 @Controller
 public class LoginController {
 	
 	@RequestMapping("/")
-	@PreAuthorize("permitAll")
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
 		return mav;
 	}
 	
-	@RequestMapping("/secret")
-	@PreAuthorize("isAuthenticated()")
-	public ModelAndView secret(ModelAndView mav, HttpServletRequest request) {
-		String user = request.getRemoteUser();
-		String msg = "This is secret page. [login by \"" + user + "\"]";
-		mav.setViewName("secret");
-		mav.addObject("title", "Secret page");
-		mav.addObject("msg", msg);
-		return mav;
-	}
-	
-	@RequestMapping("/admin")
-	@PreAuthorize("hasRole('ADMIN')")
-	public ModelAndView admin(ModelAndView mav, HttpServletRequest request) {
-		String user = request.getRemoteUser();
-		String msg = "This is admin page. [login by \"" + user + "\"]";
-		mav.setViewName("admin");
-		mav.addObject("title", "Admin page");
-		mav.addObject("msg", msg);
-		return mav;
-	}
-	
 	@RequestMapping("/login")
-	@PreAuthorize("permitAll")
 	public ModelAndView login(ModelAndView mav, @RequestParam(value="error", required=false)String error) {
 		mav.setViewName("login");
 		if (error != null) {
